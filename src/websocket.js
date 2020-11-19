707162e38a7d0f3feb6f725b9e229694de18b8d9
Vasile Maruseac <vasile.maruseac@assist.ro>
2020-11-19 09:02:46 +0200

    websockert

--- src/websocket.js
+++ src/websocket.js
@@ -1,7 +1,9 @@
 const AWS = require("aws-sdk");
 
 module.exports.handler = async (event, context, callback) => {
+  console.log(event);
   const connectionId = event.requestContext.connectionId;
+  let messageReceived = event.body;
 
   const domain = event.requestContext.domainName;
   const stage = event.requestContext.stage;
@@ -13,12 +15,33 @@ module.exports.handler = async (event, context, callback) => {
   });
 
   try {
-    await apiGatewayManagementApi
-      .postToConnection({
-        ConnectionId: connectionId,
-        Data: "test!",
-      })
-      .promise();
+    if (messageReceived) {
+      messageReceived = JSON.parse(messageReceived);
+
+      if (messageReceived.type === "connectionId") {
+        await apiGatewayManagementApi
+          .postToConnection({
+            ConnectionId: connectionId,
+            Data: JSON.stringify({
+              message: "test!",
+              type: messageReceived.type,
+              connectionId: connectionId,
+            }),
+          })
+          .promise();
+      } else {
+        await apiGatewayManagementApi
+          .postToConnection({
+            ConnectionId: connectionId,
+            Data: JSON.stringify({
+              message: "CEVA!",
+              type: messageReceived.type,
+              connectionId: connectionId,
+            }),
+          })
+          .promise();
+      }
+    }
   } catch (err) {
     console.log(`error: ${err}`);
   }
