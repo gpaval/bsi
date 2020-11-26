1cfd83a9a67c9af5731347ebfdae58a175272fa8
Vasile Maruseac <vasile.maruseac@assist.ro>
2020-11-26 08:52:27 +0200

    Websocket get data from qldb

--- src/websocket.js
+++ src/websocket.js
@@ -1,7 +1,8 @@
 const AWS = require("aws-sdk");
+const qldb = require("amazon-qldb-driver-nodejs");
 
 module.exports.handler = async (event, context, callback) => {
-  console.log(event);
+  const driver = new qldb.QldbDriver("qldb-ledger-dev");
   let connectionId = event.requestContext.connectionId;
   let messageReceived = event.body;
 
@@ -30,13 +31,24 @@ module.exports.handler = async (event, context, callback) => {
           })
           .promise();
       } else if (messageReceived.type === "registering") {
-        console.log("33: ", messageReceived);
-        connectionId = messageReceived.connectionId; // TREBUIE INCERCAT SA TRIMITI LA NOUL CONNECTION ID
+        let sql = "SELECT ";
+        for (let i = 0; i < messageReceived.requiredKeys.length; i++) {
+          sql += messageReceived.requiredKeys[i] + ", ";
+        }
+        const pos = sql.lastIndexOf(",");
+        sql = sql.substring(0, pos) + sql.substring(pos + 1);
+        sql += " FROM People WHERE email = ?";
+
+        const data = await driver.executeLambda(async (txn) => {
+          return txn.execute(sql, messageReceived.email);
+        });
+
+        connectionId = messageReceived.connectionId;
         await apiGatewayManagementApi
           .postToConnection({
             ConnectionId: connectionId,
             Data: JSON.stringify({
-              message: "3.14!",
+              message: JSON.stringify(data.getResultList()),
               type: messageReceived.type,
               connectionId: connectionId,
             }),
