ba5964485d2a677e5bc6893be0d0208057117297
gpaval <30866071+gpaval@users.noreply.github.com>
2020-12-08 12:06:04 +0200

    change table structure for vehicle maintenance, fix bug on getVehicleDetails

--- src/updateVehicleMaintenance.js
+++ src/updateVehicleMaintenance.js
@@ -3,16 +3,18 @@ var qldb = require("amazon-qldb-driver-nodejs");
 module.exports.handler = async (event, context, callback) => {
   const driver = new qldb.QldbDriver("qldb-ledger-dev");
 
-  const { vin, operation, kilometers, date, serviceName, conclusion } = event.body;
+  const { vin, operation, kilometers, serviceName, details, icon } = event.body;
 
   const maintenance = {
     VIN: vin,
     operation: operation,
     kilometers: kilometers,
-    date: date,
+    date: Math.floor(Date.now() / 1000),
     serviceName: serviceName,
-    conclusion: conclusion
+    details: details,
+    icon: icon
   };
+
   await driver.executeLambda(async (txn) => {
     const results = (await txn.execute('SELECT * FROM VehicleMaintenance WHERE VIN= ?', vin)).getResultList();
     // Check if there are any results
