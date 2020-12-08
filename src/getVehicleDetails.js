f04db911b8644b580e380fcd0c6bdae61ec9a0bd
Vasile Maruseac <vasile.maruseac@assist.ro>
2020-12-08 15:27:51 +0200

    Update getVehicleDetails&getVehicleHistory

--- src/getVehicleDetails.js
+++ src/getVehicleDetails.js
@@ -3,10 +3,21 @@ var qldb = require("amazon-qldb-driver-nodejs");
 module.exports.handler = async (event, context, callback) => {
   const driver = new qldb.QldbDriver("qldb-ledger-dev");
   const vin = event.params.querystring.vin;
+  const result = [];
 
-  const data = await driver.executeLambda(async (txn) => {
-    return txn.execute("SELECT v.*, vm.kilometers FROM Vehicle AS v, VehicleMaintenance AS vm WHERE vm.VIN = v.VIN AND v.VIN = ?", vin);
+  let data = await driver.executeLambda(async (txn) => {
+    return txn.execute("SELECT v.* FROM Vehicle AS v WHERE v.VIN = ?", vin);
   });
-  console.log(data);
-  callback(null, data);
+  result.push(...data._resultList);
+
+  data = await driver.executeLambda(async (txn) => {
+    return txn.execute(
+      "SELECT vm.kilometers FROM VehicleMaintenance AS vm WHERE vm.VIN = ?",
+      vin
+    );
+  });
+  result.push(...data._resultList);
+
+  console.log(result);
+  callback(null, result);
 };
--- src/getVehicleHistory.js
+++ src/getVehicleHistory.js
@@ -3,12 +3,21 @@ var qldb = require("amazon-qldb-driver-nodejs");
 module.exports.handler = async (event, context, callback) => {
   const driver = new qldb.QldbDriver("qldb-ledger-dev");
   const vin = event.params.querystring.vin;
+  const result = [];
 
-  const data = await driver.executeLambda(async (txn) => {
-    $query = "SELECT * FROM history(Vehicle) AS v, history(VehicleMaintenance) AS vm WHERE vm.data.VIN = v.data.VIN AND v.data.VIN = ?";
+  let data = await driver.executeLambda(async (txn) => {
+    $query = "SELECT * FROM history(Vehicle) AS v WHERE v.data.VIN = ?";
     return txn.execute($query, vin);
   });
+  result.push(...data._resultList);
 
-  console.log(data);
-  callback(null, data._resultList);
+  data = await driver.executeLambda(async (txn) => {
+    $query =
+      "SELECT * FROM history(VehicleMaintenance) AS vm WHERE vm.data.VIN = ?";
+    return txn.execute($query, vin);
+  });
+  result.push(...data._resultList);
+
+  console.log(result);
+  callback(null, result);
 };
