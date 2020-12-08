a748bc92a9766c1af5476b07d55d9cf287f5f1f9
gpaval <30866071+gpaval@users.noreply.github.com>
2020-12-08 14:27:11 +0200

    rewrite response data on vehicle details, identify vehicle by uid

--- src/getVehicleDetails.js
+++ src/getVehicleDetails.js
@@ -5,7 +5,7 @@ module.exports.handler = async (event, context, callback) => {
   const vin = event.params.querystring.vin;
 
   const data = await driver.executeLambda(async (txn) => {
-    return txn.execute("SELECT * FROM Vehicle AS v, VehicleMaintenance AS vm WHERE vm.VIN = v.VIN AND v.VIN = ?", vin);
+    return txn.execute("SELECT v.*, vm.kilometers FROM Vehicle AS v, VehicleMaintenance AS vm WHERE vm.VIN = v.VIN AND v.VIN = ?", vin);
   });
   console.log(data);
   callback(null, data);
--- src/listVehicles.js
+++ src/listVehicles.js
@@ -2,10 +2,10 @@ var qldb = require("amazon-qldb-driver-nodejs");
 
 module.exports.handler = async (event, context, callback) => {
   const driver = new qldb.QldbDriver("qldb-ledger-dev");
-  const proprietar = event.params.querystring.proprietarEmail;
+  const proprietar = event.params.querystring.uid;
 
   const data = await driver.executeLambda(async (txn) => {
-    return txn.execute("SELECT * FROM Vehicle WHERE proprietarEmail = ?", proprietar);
+    return txn.execute("SELECT * FROM Vehicle WHERE uid = ?", proprietar);
   });
 
   callback(null, data._resultList);
