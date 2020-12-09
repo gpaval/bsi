3e26a0d5e5aa06b4bf22119c8c3b1ddc96bee1e1
gpaval <30866071+gpaval@users.noreply.github.com>
2020-12-09 13:36:43 +0200

    when a new car is inserted add an registration record on vehicle maintenance

--- src/addVehicle.js
+++ src/addVehicle.js
@@ -10,21 +10,37 @@ module.exports.handler = async (event, context, callback) => {
     make,
     model,
     color,
-    proprietarEmail,
+    uid,
+    kilometers
   } = event.body;
 
   const vehicle = {
     VIN: vin,
-    licensePlateNumber: licensePlateNumber,
     year: year,
     make: make,
     model: model,
     color: color,
-    proprietarEmail,
   };
+  if (uid) {
+    vehicle.uid = uid;
+  }
+  if (licensePlateNumber) {
+    vehicle.licensePlateNumber = licensePlateNumber;
+  }
 
+  const maintenance = {
+    VIN: vin,
+    operation: 'inregistrare RAR',
+    kilometers: kilometers,
+    date: Date.now(),
+    serviceName: 'RAR',
+    details: 'Inregistrare Registrul Auto Roman',
+    icon: 'creationIcon',
+  };
   await driver.executeLambda(async (txn) => {
     txn.execute("INSERT INTO Vehicle ?", vehicle);
+    // add a maintenance record automatically after the car is inserted
+    await txn.execute("INSERT INTO VehicleMaintenance ?", maintenance);
   });
 
   callback(null, "Inserted: " + JSON.stringify(vehicle));
